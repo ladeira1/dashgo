4cbc7e1f92a36cb84575b96d1b1355d93f3f14c1
Joao Ladeira <joaoladeirag@gmail.com>
2021-03-31 20:34:55 -0300

    make header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
  
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Joao Ladeira</Text>
-        <Text color="gray.300" fontSize="small">
-          joaoladeirag@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Joao Ladeira</Text>
+          <Text color="gray.300" fontSize="small">
+            joaoladeirag@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Joao Ladeira" src="https://github.com/ladeira1.png" />
     </Flex>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 import { Searchbox } from './SearchBox'
 
 export function Header() {
+  const isScreenLarge = useBreakpointValue({ base: false, lg: true })
   return (
     <Flex 
       as="header" 
@@ -17,10 +18,10 @@ export function Header() {
       align="center" 
     >
       <Logo />
-      <Searchbox />
+      { isScreenLarge && <Searchbox /> }
       <Flex align="center" ml="auto">
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isScreenLarge} />
       </Flex>
     </Flex>
   )
