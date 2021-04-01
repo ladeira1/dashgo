0d0c7f9fcf70cd47925f987d16f7423e62c88289
Joao Ladeira <joaoladeirag@gmail.com>
2021-03-31 21:37:16 -0300

    add routes to sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,16 +1,20 @@
 import { ElementType } from "react";
-import { Link, Icon, Text, LinkProps } from "@chakra-ui/react";
+import Link from 'next/link'
+import { Link as ChakraLink, Icon, Text, LinkProps } from "@chakra-ui/react";
 
 interface NavLinkProps extends LinkProps {
   icon: ElementType
+  href: string
   children: string
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, href, children, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">{children}</Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">{children}</Text>
+      </ChakraLink>
     </Link>
   )
 }
